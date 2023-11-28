ed935ec46fd7ed1a48784dccdc05b3d9190961f4
AdamRWeinstein <adam.rick.weinstein@gmail.com>
2023-11-28 17:02:16 -0500

    Add Attacker and Defender hands, add playarea logic, add stacking logic

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 /*----- imports -----*/
-import CARDS from './constants.js'
-import ATTACK from './constants.js'
-import DEFEND from './constants.js'
+import {CARDS, ATTACK, DEFEND} from './constants.js'
 
 /*----- state variables -----*/
 
 /*----- cached elements  -----*/
 const playArea = document.getElementById("PlayArea");
-const hand = document.getElementById("Hand");
+const attackerHand = document.querySelector(".AttackerHand"); 
+const defenderHand = document.querySelector(".DefenderHand"); 
+
 
 /*----- event listeners -----*/
 
@@ -15,8 +15,8 @@ const hand = document.getElementById("Hand");
 class Game {
     constructor(){
         this.deck = this.shuffle(CARDS);
-        this.closer = this.determineCloser();
-    }
+        this.determineCloser();
+    } 
     shuffle(cards) {
         let cardsArray = Object.values(cards);
         for(let i = cardsArray.length - 1; i > 0; i--){
@@ -27,20 +27,22 @@ class Game {
     }
     deal(player, num) {
         for(let i = 0; i < num; i++){
-            player.hand.push(this.deck.shift())
+            player.addCard(this.deck.shift());
         }
     }
     determineCloser(){
+        console.log("vals")
         const vals = Object.values(this.deck);
+        
         this.closer = vals[vals.length - 1].suit
     }
 }
 
 class Player {
-    constructor(){
+    constructor(handEl, role){
         this.hand = [];
-        this.handEl = null;
-        this.role = null;
+        this.handEl = handEl;
+        this.role = role;
         this.exited = false;
     }
     setAttack(){
@@ -57,9 +59,11 @@ class Player {
         newCardEl.classList.add(`${card.rank}`);
         newCardEl.classList.add(`${card.suit}`);
         newCardEl.classList.add('Card');
+        newCardEl.classList.add(this.role)
         newCardEl.style.padding = "0px 5px 0px 5px"
         newCardEl.style.width = '15vmin';
         this.handEl.appendChild(newCardEl);
+        this.hand.push(card)
     }
     exit(){
         this.exited = true;
@@ -75,17 +79,36 @@ class Computer extends Player {
 /*----- functions -----*/
 function init(){
     let game = new Game();
-    console.log(game.closer)
-    console.log(game.deck)
+    console.log(`game.closer: ${game.closer}`)
+    console.log(`game.deck: ${game.deck}`)
+    console.log(`attackerHand: ${attackerHand}`)
+    console.log(`defenderHands: ${defenderHand}`)
+    let attacker = new Player(attackerHand, ATTACK);
+    console.log(attacker)
+    game.deal(attacker, 6);
+    console.log(attacker.hand)
+    let defender = new Player(defenderHand, DEFEND);
+    game.deal(defender, 6);
+    console.log(defender.hand)
 }
 
 init();
 
 
 /*----- Sortable JS -----*/
-new Sortable(hand, {
+new Sortable(attackerHand, {
     group: {
-        name: 'shared',
+        name: 'attackerHand',
+        pull: true,
+        put: false
+    },
+    animation: 150
+});
+
+
+new Sortable(defenderHand, {
+    group: {
+        name: 'defenderHand',
         pull: true,
         put: false
     },
@@ -94,10 +117,36 @@ new Sortable(hand, {
 
 new Sortable(playArea, {
     group: {
-        name: 'shared',
+        name: 'playArea',
         pull: false,
-        put: true
+        put: function(to, from) {
+            return (to.el.children.length < 6) && from.el.classList.contains("AttackerHand")
+        }
     },
     animation: 150,
-    sort: false
+    sort: false,
+    swapThreshold: 0,
+    onAdd: function(evt) {
+        const attackCard = document.createElement("div");
+        attackCard.classList.add("AttackCard")
+        attackCard.appendChild(evt.item);
+        evt.to.appendChild(attackCard)
+        
+        new Sortable(attackCard, {
+            group: {
+                name: 'attackCard',
+                pull: false,
+                put: function(to, from) {
+                    return (to.el.children.length === 1) && (from.el.classList.contains("DefenderHand"))
+                },
+            },
+            sort: false,
+            onAdd: function(evt) {
+                evt.item.classList.add("DefenderCard")
+                evt.to.appendChild(evt.item)
+            },
+            direction: "vertical",
+            swapThreshold: 0
+        });
+    }
 });
